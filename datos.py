de9d53977e1564b1f16b81d268b1ca7fd837ecd3
Iupfa1111 <an.ro.ib@hotmail.com>
2026-08-20 17:12:30 -0300

    Add edificios_db and puntos_seguros data structures

---
+++ datos.py
@@ -0,0 +1,33 @@
+# datos.py
+
+edificios_db = {
+    "Edificio Kavanagh": {"dir": "Florida 1065, Retiro", "alt": "120 m", "acc": "Principal / Cocheras", "coords": "-34.5922, -58.3753"},
+    "Palacio Barolo": {"dir": "Av. de Mayo 1370, Monserrat", "alt": "100 m", "acc": "Principal / Carga", "coords": "-34.6095, -58.3860"},
+    "Teatro Colón": {"dir": "Cerrito 628, San Nicolás", "alt": "30 m", "acc": "Libertad / Tucumán", "coords": "-34.6011, -58.3816"},
+    "Congreso de la Nación": {"dir": "Av. Entre Ríos 50, Balvanera", "alt": "80 m", "acc": "Principal / Protocolar", "coords": "-34.6099, -58.3916"},
+    "Casa Rosada": {"dir": "Balcarce 50, Monserrat", "alt": "24 m", "acc": "Balcarce 24 / Explanada", "coords": "-34.6081, -58.3702"},
+    "Torre YPF": {"dir": "Macacha Güemes 515, Pto Madero", "alt": "160 m", "acc": "Dique 4", "coords": "-34.6031, -58.3639"},
+    "Facultad de Derecho UBA": {"dir": "Av. Figueroa Alcorta 2263", "alt": "35 m", "acc": "Escalinata", "coords": "-34.5833, -58.3897"},
+    "CCK": {"dir": "Sarmiento 151", "alt": "40 m", "acc": "Sarmiento / Alem", "coords": "-34.6044, -58.3694"},
+    "Estación Retiro Mitre": {"dir": "Av. Dr. Ramos Mejía 1302", "alt": "30 m", "acc": "Hall Central", "coords": "-34.5903, -58.3742"},
+    "Estación Constitución": {"dir": "Av. Brasil 1152", "alt": "30 m", "acc": "Hall Principal", "coords": "-34.6281, -58.3814"},
+}
+
+puntos_seguros = {
+    "Hosp. Argerich": (-34.6366, -58.3639),
+    "Hosp. Ramos Mejía": (-34.6152, -58.4079),
+    "Hosp. Fernández": (-34.5833, -58.4069),
+    "Hosp. Garrahan": (-34.6328, -58.3986),
+    "Comisaría Comunal 1": (-34.5842, -58.3695),
+    "Comisaría Comunal 2": (-34.5911, -58.3923),
+    "Comisaría Comunal 4": (-34.6419, -58.4028),
+    "Comisaría Comunal 14": (-34.5812, -58.4136),
+    "PFA Depto. Central": (-34.6146, -58.3848),
+    "PFA Supt. Investigaciones": (-34.6135, -58.3912),
+    "PFA Delitos Tecnológicos": (-34.5778, -58.4065),
+    "PFA División FF.CC. Mitre": (-34.5903, -58.3742),
+    "PFA División FF.CC. Sarmiento": (-34.6095, -58.4078),
+    "PFA División FF.CC. Roca": (-34.6281, -58.3814),
+    "PFA Terminal Ómnibus": (-34.5885, -58.3725),
+    "PFA Supt. Bomberos": (-34.6045, -58.3810),
+}
